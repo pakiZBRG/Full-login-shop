ec2d06b6b533f87c2a40c5c0fd09b63347e88a83
pakiZBRG <pavlovicnikola511@gmail.com>
2021-03-10 20:26:01 +0100

    deployment act10

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ mongoose.connect(process.env.MONGO_URI, {
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/public'));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '/client/public'));
+        res.sendFile(path.join(__dirname, '/client/public/index.html'));
     });
 }
 
