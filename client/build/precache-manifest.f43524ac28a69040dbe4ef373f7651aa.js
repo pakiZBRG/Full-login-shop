0d550dcb9a2a3d22ad867df9282f7ad7464fba50
pakiZBRG <pavlovicnikola511@gmail.com>
2021-03-10 19:16:43 +0100

    deployed

---
+++ client/build/precache-manifest.f43524ac28a69040dbe4ef373f7651aa.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ae1d8e0e414123fb3cdc04b478d2f5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aadfdf5080dd25f8bc66",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "63550ecef04b4f999889",
+    "url": "/static/css/main.07cf42dd.chunk.css"
+  },
+  {
+    "revision": "aadfdf5080dd25f8bc66",
+    "url": "/static/js/2.bead542a.chunk.js"
+  },
+  {
+    "revision": "0fef1ed8976d61fc4163ae06e66c7d37",
+    "url": "/static/js/2.bead542a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63550ecef04b4f999889",
+    "url": "/static/js/main.d9435fb8.chunk.js"
+  },
+  {
+    "revision": "0dc9215c4b0a17c8c874",
+    "url": "/static/js/runtime-main.7bf3d854.js"
+  },
+  {
+    "revision": "527a422559222960cf2b547822839b5a",
+    "url": "/static/media/activation.527a4225.jpg"
+  },
+  {
+    "revision": "6e601c09e57ce0b2b57052a8099cc7ec",
+    "url": "/static/media/addproduct.6e601c09.png"
+  },
+  {
+    "revision": "0d4418d42964423a0c2e9f240db96cef",
+    "url": "/static/media/editProduct.0d4418d4.jpg"
+  },
+  {
+    "revision": "5b03f3b3233eaecb6a54371b914ce760",
+    "url": "/static/media/forgotpassword.5b03f3b3.jpg"
+  },
+  {
+    "revision": "24e18e7d92e0b402cb52ee82ddaddd1a",
+    "url": "/static/media/login.24e18e7d.jpg"
+  },
+  {
+    "revision": "624ce12d69bc3508324cef9084a6434c",
+    "url": "/static/media/register.624ce12d.jpg"
+  },
+  {
+    "revision": "e5585993411dba0439860ddd4a9c3410",
+    "url": "/static/media/resetpassword.e5585993.jpg"
+  }
+]);
