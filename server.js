e40b0f5ad069ad0da5c5c25607c7b2803f9fe220
pakiZBRG <pavlovicnikola511@gmail.com>
2021-03-15 15:04:44 +0100

    changed path for rendering pages inproduction mode

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ mongoose.connect(process.env.MONGO_URI, {
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
     });
 }
 
